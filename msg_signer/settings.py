30f5f181b72235b293a1d0c6fd99ca6ac2a8be58
David Simek <david.simek1998@gmail.com>
2022-05-09 19:45:57 +0200

    added ethereum.enabled()

--- msg_signer/settings.py
+++ msg_signer/settings.py
@@ -29,7 +29,7 @@ DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', default=[
     'msg-signer.herokuapp.com',
-    '127.0.0.1:8000',
+    '127.0.0.1',
 ])
 
 
